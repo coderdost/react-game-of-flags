1bc6c060780e881a9d333a3f92e582763672f092
Abhishek Rathore <rathore.abhishek87@gmail.com>
2021-09-07 17:37:39 +0530

    Game of Flags

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
+import nations from './nations';
+import 'flag-icon-css/css/flag-icon.css';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [country, setCountry] = useState([]);
+  const [flagCountry, setFlagCountry] = useState({});
+  const [score, setScore] = useState({ total: 0, correct: 0, incorrect: 0 });
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [selected, setSelected] = useState({});
+
+  const generateRandomNations = () => {
+    let ct = [];
+    for (let i = 0; i < 4; i++) {
+      const r = Math.floor(Math.random() * nations.length);
+      ct.push(nations[r]);
+    }
+
+    setCountry(ct);
+    const index = Math.floor(Math.random() * 4);
+    setFlagCountry(ct[index]);
+  };
+
+  const checkAnswer = (country) => {
+    setSelected(country);
+    if (country.name === flagCountry.name) {
+      setScore({
+        ...score,
+        correct: score.correct + 1,
+        total: score.total + 1,
+      });
+    } else {
+      setScore({
+        ...score,
+        incorrect: score.incorrect + 1,
+        total: score.total + 1,
+      });
+    }
+    setShowAnswer(true);
+    setTimeout(() => {
+      setShowAnswer(false);
+      nextQuestion();
+    }, 5000);
+  };
+  const nextQuestion = () => {
+    generateRandomNations();
+  };
+
+  useEffect(() => {
+    generateRandomNations();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h4>
+          Total : {score.total} / Correct : {score.correct} / InCorrect :{' '}
+          {score.incorrect}
+        </h4>
+      </div>
+      {flagCountry.name ? (
+        <span
+          className={`flag-icon flag-icon-${flagCountry[
+            'alpha-2'
+          ].toLowerCase()}`}
+        ></span>
+      ) : null}
+
+      <div>
+        {country.map((c) => (
+          <button onClick={(e) => checkAnswer(c)}>{c.name}</button>
+        ))}
+      </div>
+
+      <div>
+        {showAnswer ? (
+          <h2
+            className={
+              flagCountry.name === selected.name ? 'correct' : 'incorrect'
+            }
+          >
+            Correct : {flagCountry.name}
+          </h2>
+        ) : null}
+      </div>
     </div>
   );
 }
